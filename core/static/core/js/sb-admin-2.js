6ba133400933ce946e120a061849a98a4e16c79b
parisaqomi <parisaq97@gmail.com>
2019-06-27 17:18:01 +0430

    js error fixed

--- core/static/core/js/sb-admin-2.js
+++ core/static/core/js/sb-admin-2.js
@@ -46,12 +46,5 @@
     e.preventDefault();
   });
 
-  //cs
-  $(".tbtn").click(function () {
-    $(this).parents(".custom-table").find(".toggler1").removeClass("toggler1");
-    $(this).parents("tbody").find(".toggler").addClass("toggler1");
-    $(this).parents(".custom-table").find(".fa-minus-circle").removeClass("fa-minus-circle");
-    $(this).parents("tbody").find(".fa-plus-circle").addClass("fa-minus-circle");
-});
 
 })(jQuery); // End of use strict
