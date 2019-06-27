6fe06cc006f85af282a887424aa4b3e1f7167344
parisaqomi <parisaq97@gmail.com>
2019-06-27 18:26:35 +0430

    icons changes and course list fixed

--- core/static/core/js/demo/chart-pie-demo.js
+++ core/static/core/js/demo/chart-pie-demo.js
@@ -7,26 +7,26 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["گذشته", "munde"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [55 , 45],
+      backgroundColor: ['#4e73df' ,'#4e7388'],
+      hoverBackgroundColor: ['#2e59d9','#4e73df'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
   options: {
     maintainAspectRatio: false,
-    tooltips: {
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-    },
+    // tooltips: {
+    //   backgroundColor: "rgb(255,255,255)",
+    //   bodyFontColor: "#858796",
+    //   borderColor: '#dddfeb',
+    //   borderWidth: 1,
+    //   xPadding: 15,
+    //   yPadding: 15,
+    //   displayColors: false,
+    //   caretPadding: 10,
+    // },
     legend: {
       display: false
     },
